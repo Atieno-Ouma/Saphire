ad4fde52fba497803b563bee28488e3ae0fc45b4
Atieno-ouma <atienooumavee@gmail.com>
2021-11-17 20:55:22 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,79 @@
+# This is a sample Python script.
+import depsies
+
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+names = input("Enter names separated by commas: ").title().split(",")
+assignments = input("Enter assignment counts separated by commas: ").split(",")
+grades = input("Enter grades separated by commas: ").split(",")
+
+message = "Hi {},\n\nThis is a reminder that you have {} assignments left to \nsubmit before you can graduate. You're current grade is {} and can increase \nto {} if you submit all assignments before the due date.\n\n"
+
+for name, assignment, grade in zip(names, assignments, grades):
+    send =message.format(name, assignment, grade, int(grade) + int(assignment)*2)
+    print(send)
+# Press the green button in the gutter to run the script.
+
+def party_planner(cookies, people):
+    leftovers = None
+    num_each = None
+    # TODO: Add a try-except block here to
+    #       make sure no ZeroDivisionError occurs.
+    try:
+        num_each = cookies // people
+        leftovers = cookies % people
+    except ZeroDivisionError:
+        print("Oops, you entered 0 people will be attending.")
+        print("Please enter a good number of people for a party.")
+
+    return(num_each, leftovers)
+
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+#try:
+    # some code
+#except Exception as e:
+   # some code
+  # print("Exception occurred: {}".format(e))
+
+f = open('sample.txt', 'r')
+file_data = f.read()
+f.close()
+
+f = open('sample.txt', 'a')
+f.write(send)
+f.close()
+
+with open('sample.txt') as f:
+    print(f.read(7))
+    print(f.readline())
+    print(f.readline())
+
+camelot_lines = []
+with open("camelot.txt") as f:
+    for line in f:
+        camelot_lines.append(line.strip())
+
+print(camelot_lines)
+def create_cast_list(filename):
+    cast_list = []
+    with open(filename) as f:
+        for line in f:
+            name = line.split(",")[0]
+            cast_list.append(name)
+
+    return cast_list
+
+cast_list = create_cast_list('flying_circus_cast.txt')
+for actor in cast_list:
+    print(actor)
